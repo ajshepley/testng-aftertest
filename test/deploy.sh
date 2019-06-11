270e1ceef79b5ec64d394eab9d30bdd5902c2b95
Adam Shepley <adam.shepley@shopify.com>
2019-06-11 15:30:36 -0400

    Added another failing commit, added date logging.

--- test/deploy.sh
+++ test/deploy.sh
@@ -4,9 +4,13 @@ echo "Deploying!"
 
 echo "Sleeping 5m..."
 
-curl https://httpstat.us/200?sleep=300000
+date
+
+curl https://httpstat.us/200?sleep=300000 --max-time 300
 
 echo "Done sleeping!"
+
+date
 
 exit 0
 
